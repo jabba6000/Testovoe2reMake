d3b9bb1e65c164f91edde41f62ca05316f8fd2ee
Uri Fuholichev <Uri@Uris-Mac-mini.local>
2016-10-10 21:56:44 +0300

    0.01 clear project

---
+++ TestovoeRest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestovoeRest
+//
+//  Created by Uri Fuholichev on 10/10/16.
+//  Copyright © 2016 Andrei Karpenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
